4cf346d2fcb0cbeb4ea4701ee67e159216834db7
Arthur Tira <12863951+arthurtira@users.noreply.github.com>
2022-03-07 16:20:50 +0000

    Update SqsMessageProducer.java

--- src/main/java/com/arthurtira/sbs/producer/SqsMessageProducer.java
+++ src/main/java/com/arthurtira/sbs/producer/SqsMessageProducer.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.stereotype.Component;
-import sun.security.krb5.internal.crypto.HmacSha1Aes128CksumType;
 
 import java.util.Map;
 
